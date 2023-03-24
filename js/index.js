770019de1aef24b25f6441c55d38ed7170c6340d
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-03-23 23:39:30 -0400

    added copy functionality

--- js/index.js
+++ js/index.js
@@ -18,6 +18,21 @@ selectScheme.addEventListener('input', (e) => {
     colorScheme(colorValue, schemeValue) //run function to update color palette
 })
 
+//copy color in column
+colorSchemes.addEventListener('click', (e) => {
+    const clickedColumn = e.target.style["background-color"]
+    const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+    columnColor = rgb2hex(clickedColumn);
+    copyColor(columnColor);
+    //rgb2hex source: https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+})
+
+//copy color in hex text
+footer.addEventListener('click', (e) => {
+    const hexText = (e.target.innerText).toLowerCase();
+    copyColor(hexText);
+})
+
 function colorScheme() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${schemeValue}&count=5`)
     .then( response => response.json() )
@@ -35,6 +50,18 @@ function colorScheme() {
             `;
         })
     })
+}
+
+//copy color to clipboard
+function copyColor(colorData) {
+        navigator.clipboard.writeText(colorData).then(
+            () => {
+                console.log(`Copied: ${colorData}`)    
+            }, () => {
+                console.log('Copy Failed')
+            }
+        )
+    
 }
 
 
